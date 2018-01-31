cc89beab54283f31eccd3eb132799cc631d742f6
PierrickRauby <pierrick.rauby@gadz.org>
2018-01-31 12:15:55 -0500

    Old code replaced by code used on the BBB

--- Reading_Device_ID/SPI/mainSPI.c
+++ Reading_Device_ID/SPI/mainSPI.c
@@ -1,9 +1,9 @@
 /*
  * Source Modified by Pierrick Rauby <PierrickRauby - pierrick.rauby@gmail.com>
- * Attempt to duplicate Greg Raven pruadc1 SPI code for MCP3008
  * Based on the examples distributed by TI
  * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
-
+ *
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -45,7 +45,7 @@
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
-uint32_t spiCommand;
+uint8_t spiCommand;
 #define PULSEWIDTH 300
 
 /* Host-0 Interrupt sets bit 30 in register R31 */
@@ -90,61 +90,57 @@ void main(void)
 		CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
 		/* Receive all available messages, multiple messages can be sent per kick */
 		while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
-					//—————————————————————————SPI Capture Loop———————————————————————————
-					//——————————————————Initializing the SPI communication————————————————
-					uint32_t data = 0x00000000; // Incoming data stored here.
-					                          	//  The data out line is connected to R30 bit 1.
-					uint16_t dataCounter = 0; // Used to load data transmission buffer payloadOut;
+					//variables initialization
+					uint16_t data = 0x00; // Incoming data stored here.
+					spiCommand = 0x30;
+					// int RW= 1; // RW=1 for reading
+					// if (RW){
+					// spiCommand= spiCommand | (1<<7);
+					// }
 					__R30 = 0x00000000;//  Clear the output pin.
-
-					__R30 = __R30 | (1 << 5); // Initialize chip select HIGH. __R30 = 0x00010000;
-					__delay_cycles(100000000); //  Allow chip to stabilize.
-					//————————————Transmitting the address we want to receive data from———
-					//spiCommand is the MOSI preamble; must be reset for each sample.
-					    spiCommand = 0x00000000;    // All 0 to have the Device ID
-					    data = 0x00000000;          //  Initialize data.
-					    __R30 = __R30 & 0xFFFFFFDF; //  Chip select to LOW P9.27
-					/* au dessus: 0xFFFFFFDF => que des 1 sauf en bit n°5 ou il y a un 0 donc avec
-					l'opérateur & on obient  0x00000000 => le bit 5 de __R30 est passé à LOW */
-					    __R30 = __R30 & 0xFFFFFFFB; //  Clock to LOW   P9.30
-					//  Start-bit is HIGH.  The Start-bit is manually clocked in here.
-					/* au dessus: 0xFFFFFFFB => que des 1 sauf en bit n°2 où il y a un 0 donc avec
-					l'opérateur & on obtient 0x00000000 => le bit 5 de _R30 passe à 0 */
-					    __R30 = __R30 | (1 << 1);   //  Set a 0x10 on MOSI (the Start-bit)
-					/* On met le bit 1 du registre _R30 sur 1 pour lancer MOSI*/
-					    __delay_cycles(PULSEWIDTH); //  Delay to allow settling.
-					    __R30 = __R30 | 0x00000004; //  Clock goes high; latch in start bit.
-					/* passe le bit n°2 a 1 ce qui active la clock */
-					    __delay_cycles(PULSEWIDTH); //  Delay to allow settling.
-					    __R30 = __R30 & 0xFFFFFFFB; //  Clock to LOW   P9.30
-					// The Start-bit cycle is completed.
-					//——————————–––——Receiving data from the address given above——————————
-					for (i = 0; i < 16; i = i + 1) { //  Inner single sample loop
-					     //  The first action will be to transmit on MOSI by shifting out
-					     //  spiCommand variable.
-					     if (spiCommand >> 31)       //  If the MSB is 1
-					       __R30 = __R30 | (1 << 1); //  Set a 0x10 on MOSI
-					     else
-					       __R30 = __R30 & (0xFFFFFFFD); //  All 1s except bit 1
-					     spiCommand = spiCommand << 1;   // Shift left one bit (for next cycle).
-					     __delay_cycles(PULSEWIDTH);
-					     __R30 = __R30 | 0x00000004; //  Clock goes high; bit set on MOSI.
-					     __delay_cycles(PULSEWIDTH); //  Delay to allow settling.
-					     //  The data needs to be "shifted" into the data variable.
-					     data = data << 1;           // Shift left; insert 0 at lsb.
-					     __R30 = __R30 & 0xFFFFFFFB; //  Clock to LOW   P9.30
-					     if (__R31 & (1 << 3)) //  Probe MISO data from ADC.
-					       data = data | 1; //if one received LSB set to 1
-					     else
-					       data = data & 0xFFFFFFFE; //else LSB set to 0
-					   } // *****End of 16 cycles loop****
-				 __R30 = __R30 | 1 << 5; //  Chip select to HIGH
-				 payload[dataCounter]=(int8_t)data;
-				 dataCounter = dataCounter + 1;
-				 if (dataCounter == 1) {
-				   pru_rpmsg_send(&transport, dst, src, payload, 490);
-				   dataCounter = 0;
-					   }
+					//start R/W bit
+					__R30 = __R30 | (1 << 5); //chip select HIGH. __R30 = 0x00100000;
+					__delay_cycles(100000000); // Delay
+					__R30 = __R30 & 0xFFFFFFDF; //  CS-> LOW P9.27 r30_5
+					__R30 = __R30 & 0xFFFFFFFB; //  Clock -> LOW P9.30 r30_2
+					__R30 = __R30 | (1 << 1);   //  MOSI -> HIGH P9.29 r30_1
+					__delay_cycles(PULSEWIDTH); //  Delay
+					__R30 = __R30 | (1<<2); //  Clock -> HIGH P9.30 r30_2
+					__delay_cycles(PULSEWIDTH); //  Delay
+					__R30 = __R30 & 0xFFFFFFFB; //  Clock -> LOW P9.30 r30_2
+					__R30 = __R30 & (0xFFFFFFFD);  //  MOSI -> LOW P9.29 r30_1
+					//loop for 15 last bits
+					for (i = 0; i < 15; i ++) {
+								//shifting
+								//data<<=1;
+								//spiCommand <<=1;
+								// Writting on MOSI
+						    if (spiCommand & (1<<7)){  //  If the spiCommand MSB 1
+						       __R30 = __R30 | (1 << 1); //  MOSI -> HIGH P9.29 r30_1
+						    }
+						    else{
+						       __R30 = __R30 & (0xFFFFFFFD);  //  MOSI -> LOW P9.29 r30_1
+						    }
+								//Clock toggling
+						    __delay_cycles(PULSEWIDTH); //  Delay
+						    __R30 = __R30 | (1<<2); //  Clock -> HIGH P9.30 r30_2
+								// Reading on MISO
+								if (__R31 & (1 << 3)){ //MISO read P9.28 r31_3
+									 data |= 1;} // 1-> data's LBS
+								__delay_cycles(PULSEWIDTH); //  Delay
+						    __R30 = __R30 & 0xFFFFFFFB; //  Clock -> LOW P9.30 r30_2
+								//shifting
+								data<<=1;
+								spiCommand <<=1;
+ 							 }
+					__delay_cycles(PULSEWIDTH); //  Delay
+					__R30 = __R30 | (1<<2); //  Clock -> HIGH P9.30 r30_2
+					if (__R31 & (1 << 3)){ //MISO read P9.28 r31_3
+					 data |= 1;} // 1-> data's LBS
+					__delay_cycles(PULSEWIDTH); //  Delay
+				 	__R30 = __R30 | 1 << 5; //  Chip select to HIGH
+				 	payload[0]= (uint8_t)data;
+				 	pru_rpmsg_send(&transport, dst, src, payload, 2);
 				}
     }
   }
